5aefe6e717bdd9341af1bb73bf648e783d40a420
longwei <1175978196@qq.com>
2019-08-02 00:22:45 +0800

    登录token认证

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var userSchema = new mongoose.Schema({
  "userId": String,
  "userName": String,
  "userPwd": String,
+ "token":""
 })
 
 module.exports = mongoose.model('User',userSchema);//User是模型名字  users是关联的数据库，当数据库名带s时，可以省略
--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,25 @@ var router = express.Router();
 var User = require('./../models/user')
 // 连接mongodb数据库
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/demo',{useMongoClient:true})
 
-mongoose.connection.on("connected",function(){
-    console.log("mongodb连接成功")
+var jwt = require('jsonwebtoken'); // 使用jwt签名
+mongoose.connect('mongodb://localhost:27017/zhimi', {
+  useMongoClient: true
 })
-mongoose.connection.on("error",function(){
-    console.log("mongodb连接失败")
+
+mongoose.connection.on("connected", function () {
+  console.log("mongodb连接成功")
 })
-mongoose.connection.on("disconnected",function(){
-    console.log("mongodb连接disconnected")
+mongoose.connection.on("error", function () {
+  console.log("mongodb连接失败")
 })
-/* GET users listing. */
-router.post('/login', function(req, res, next) {
-  //登录接口  查找某一个用户，根据用户密码   doc 是用户文档  
+mongoose.connection.on("disconnected", function () {
+  console.log("mongodb连接disconnected")
+})
+
+// 登录接口
+router.post('/login', function (req, res, next) {
+  //  查找某一个用户，根据用户密码   doc 是用户文档  
   var param = {
     userName: req.body.userName,
     userPwd: req.body.userPwd
@@ -25,31 +30,86 @@ router.post('/login', function(req, res, next) {
     if (err) {
       res.json({
         status: "1",
-        msg: err.message
+        msg: "登录失败，" + err
       });
     } else {
-      if (doc) {
-        // 写cookie
-        res.cookie("userId", doc.userId, {
-          path: "/",
-          maxAge: 1000 * 60 * 60
-        });
-        res.cookie("userName", doc.userName, {
-          path: "/",
-          maxAge: 1000 * 60 * 60
-        })
-        // 存到session  需要安装插件才能用的 express-session
-        // req.session.user = doc
-        res.json({
-          status: "1",
-          msg: "",
-          result: {
-            userName: doc.userName
-          }
-        })
-      }
+      // 暂时不用cookie的方式做认证
+      // res.cookie("userId", doc.userId, {
+      //   path: "/",
+      //   maxAge: 1000 * 60 * 60
+      // });
+      // res.cookie("userName", doc.userName, {
+      //   path: "/",
+      //   maxAge: 1000 * 60 * 60
+      // })
+      let content = param; // 要生成token的主题信息
+      let secretOrPrivateKey = "long" // 这是加密的key（密钥） 
+      let token = jwt.sign(content, secretOrPrivateKey, {
+        expiresIn: 60 * 1 * 1 // 1分钟过期
+      });
+      doc.token = token
+      doc.save(function (err1, doc1) {
+        if (err1) {
+          res.json({
+            status: "1",
+            msg: "登录失败," + err1
+          })
+        } else {
+          res.json({
+            status: "0",
+            msg: "登录成功",
+            result: doc1.token
+          })
+        }
+      })
     }
 
+  })
+});
+
+// 校验接口
+router.post('/checkLogin', function (req, res, next) {
+  var token = req.body.token
+  var param = {
+    token:token
+  }
+
+  User.findOne(param, function (err, doc) {
+    if (err) {
+      res.json({
+        status: "1",
+        msg: "token不存在或者失效，" + err
+      });
+    } else {
+      jwt.verify(token, "long", function (err, decode) {
+        if (err) { //  时间失效的时候/ 伪造的token          
+          res.json({
+            status: "1",
+            msg: "token不存在或者失效",
+            result: doc?doc.token:""
+          })
+        } else {
+          res.json({
+            status: "0",
+            msg: "token正常",
+            result: doc.userName
+          })
+        }
+      })
+
+      //     res.json({
+      //       status: "1",
+      //       msg: "token不存在或者失效",
+      //       result:doc
+      //     })
+      //   }else{
+      //   res.json({
+      //     status: "0",
+      //     msg: "token正常",
+      //     result:doc.userName
+      //   })
+      // }
+    }
   })
 });
 
