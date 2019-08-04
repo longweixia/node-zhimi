d0f8f25e9b4db54f9bf29760b4e296f55b709ca8
longwei <1175978196@qq.com>
2019-08-04 23:32:28 +0800

    完成登录注册页面

--- routes/users.js
+++ routes/users.js
@@ -42,26 +42,36 @@ router.post('/login', function (req, res, next) {
       //   path: "/",
       //   maxAge: 1000 * 60 * 60
       // })
-      let content = param; // 要生成token的主题信息
-      let secretOrPrivateKey = "long" // 这是加密的key（密钥） 
-      let token = jwt.sign(content, secretOrPrivateKey, {
-        expiresIn: 60 * 1 * 1 // 1分钟过期
-      });
-      doc.token = token
-      doc.save(function (err1, doc1) {
-        if (err1) {
-          res.json({
-            status: "1",
-            msg: "登录失败," + err1
-          })
-        } else {
-          res.json({
-            status: "0",
-            msg: "登录成功",
-            result: doc1.token
-          })
-        }
-      })
+      // 注意需要doc是否存在，如果不存在这个用户那么doc是null,doc1也是null,doc1.token就会报错
+      if(!doc){
+        res.json({
+          status: "1",
+          msg: "用户名或密码错误"
+          // result: doc
+        })
+      }else{
+        let content = param; // 要生成token的主题信息
+        let secretOrPrivateKey = "long" // 这是加密的key（密钥） 
+        let token = jwt.sign(content, secretOrPrivateKey, {
+          expiresIn: 60 * 1 * 1 // 1分钟过期
+        });
+        doc.token = token
+        doc.save(function (err1, doc1) {
+          if (err1) {
+            res.json({
+              status: "1",
+              msg: "登录失败," + err1
+            })
+          } else {
+            res.json({
+              status: "0",
+              msg: "登录成功",
+              result: doc1.token
+            })
+          }
+        })
+      }
+
     }
 
   })
@@ -81,36 +91,105 @@ router.post('/checkLogin', function (req, res, next) {
         msg: "token不存在或者失效，" + err
       });
     } else {
-      jwt.verify(token, "long", function (err, decode) {
+      jwt.verify(token, "long", function (err, doc1) {
         if (err) { //  时间失效的时候/ 伪造的token          
           res.json({
             status: "1",
-            msg: "token不存在或者失效",
-            result: doc?doc.token:""
+            msg: "token不存在或者失效"
+            // result: doc?doc.token:""
           })
         } else {
           res.json({
             status: "0",
             msg: "token正常",
-            result: doc.userName
+            result: doc1.userName
           })
         }
       })
-
-      //     res.json({
-      //       status: "1",
-      //       msg: "token不存在或者失效",
-      //       result:doc
-      //     })
-      //   }else{
-      //   res.json({
-      //     status: "0",
-      //     msg: "token正常",
-      //     result:doc.userName
-      //   })
-      // }
     }
   })
+});
+
+// 退出接口
+router.post('/loginOut', function (req, res, next) {
+  var param = {
+    userName:req.body.userName
+  }
+
+  User.findOne(param, function (err, doc) {
+    if (err) {
+      res.json({
+        status: "1",
+        msg: "退出失败" + err
+      });
+    } else {
+   doc.token="";
+   doc.save(function (err1, doc1) {
+    if (err1) {
+      res.json({
+        status: "1",
+        msg: "退出失败," + err1
+      })
+    } else {
+      res.json({
+        status: "0",
+        msg: "退出成功"
+      })
+    }
+  })
+    }
+  })
+});
+// 注册接口
+router.post('/register', function (req, res, next) {
+  var param = {
+    userName:req.body.userName,
+    userPwd:req.body.userPwd
+  }
+  let content = param; // 要生成token的主题信息
+  let secretOrPrivateKey = "long" // 这是加密的key（密钥） 
+  let token = jwt.sign(content, secretOrPrivateKey, {
+    expiresIn: 60 * 1 * 1 // 1分钟过期
+  });
+  var params = {
+    userId:Math.floor(Math.random()*100),
+    userName:req.body.userName,
+    userPwd:req.body.userPwd,
+    token:token
+  }
+// 先判断用户名是否存在
+  User.findOne({userName:req.body.userName}, function (err, doc) {
+    if (err) {
+      res.json({
+        status: "1",
+        msg: "注册失败" + err
+      });
+    } else {
+       // 不存在才创建用户
+      if(!doc){
+        User.create(params, function (err1, doc1) {
+          if (err1) {
+            res.json({
+              status: "1",
+              msg: "注册失败" + err1
+            });
+          } else {
+            res.json({
+              status: "0",
+              msg: "注册成功",
+              result:doc1.token
+            });
+          }
+        })
+      }else{ // 存在就报错，用户存在
+        res.json({
+          status: "1",
+          msg: "用户名存在"
+        });
+      }
+    }
+  })
+
 });
 
 module.exports = router;
