5d21620ae6b2b78b187e005f29ca890403ab6a3a
longwei <1175978196@qq.com>
2020-03-25 23:49:14 +0800

    收藏接口

--- routes/collections.js
+++ routes/collections.js
@@ -4,29 +4,85 @@ var Collections = require('./../models/collection')
 var malls = require('./../models/mall')
 let fs = require("fs");
 
-// 收藏简历模板
+// 收藏/取消简历模板
 router.post('/resume', function(req, res, next) {
     var param = {
         userName: req.body.userName
     }
     let mallId = req.body.mallId
+    
     Collections.findOne(param, function(err, doc) {
+        // 定义添加收藏方法
+    function collect(){
+        // 当resumeList为空数组，或者mallId不存在resumeList数组中，就添加
+        doc.resumeList = doc.resumeList.concat({
+            mallId:mallId
+        })
+        doc.save(function(err3, doc3) {
+            if(err3){
+                res.json({
+                    status: "1",//0表示收藏成功，1表示操作失败，2表示删除成功
+                    msg: "提交失败" + err3
+                });
+            }else{
+                res.json({
+                    status: "0",
+                    msg: "收藏成功"
+                });
+            }
+        })
+
+    }
         // 如果集合不存在，创建集合
         if(doc==null){
-            let resumeList = [].push(mallId)
+            // 这里我原来是用let resumeList = [].push(1),打印发现resumeList是值是1，不是数组，什么情况
+            let resumeList = [].concat({mallId:mallId})
             Collections.create({"userName": req.body.userName,"resumeList":resumeList}, function(err1, doc1) {
                 if (err1) {
                     res.json({
                         status: "1",
-                        msg: "提交失败" + err1
+                        msg: "收藏失败" + err1
                     });
                 } else {
                     res.json({
                         status: "0",
-                        msg: "提交成功"
+                        msg: "收藏成功"
                     });
                 }
             })
+        }else{
+            // 如果集合存在
+            let content = doc.resumeList
+            // 如果resumeList是空数组，说明，没有收藏任何模板，添加模板就行了
+            if(content==""){
+                collect()
+            }else{
+                // 如果resumeList有值，说明已经收藏模板，那要查询当前模板是否已经存在了
+                for(var i=0;i<content.length;i++){
+                    // 如果id已经存在，执行删除操作
+                    if(mallId==content[i].mallId){
+                        // 删除doc集合中，resumeList数组里面mallId属性值为mallId这一项
+                        doc.update({$pull:{resumeList:{mallId:mallId}}},function(err2,doc2){
+                            if(err2){
+                                res.json({
+                                    status: "1",
+                                    msg: "取消收藏失败"+err2,
+
+                                });
+                            }else{
+                                res.json({
+                                    status: "2",
+                                    msg: "取消收藏成功",
+
+                                });
+                            }
+                    })
+                    return false
+                }
+                }
+                // 如果id存在，上面的return false会阻止这里的代码执行，所以这里写id不存在，进行添加的代码
+                collect()
+            }
         }
     })
 });
@@ -37,7 +93,7 @@ router.get('/getResume', function(req, res, next) {
         userName: req.param("userName"),
     }
     Collections.findOne(param, function(err, doc) {
-         //如果没有保存简历，返回空
+         //如果没有收藏模板简历，返回空
           if(doc==[]||doc==null||doc==""){
               res.json({
               status: "1",
@@ -46,21 +102,32 @@ router.get('/getResume', function(req, res, next) {
           });
           return false
           }
-        //如果有，找到每个简历的图片id，然后通过id去malls集合去找   
+        //如果有，找到每个简历的图片id，然后通过id去malls集合去匹配  
           let imgList=[];
           let content = doc.resumeList
           malls.find(function(err1, doc1) {
-              console.log(doc1.mallId,doc1[0],"doc1")
+              if(err1){
+                res.json({
+                    status: "1",
+                    msg: "查询失败"+err1,
+                    result: ""
+                });
+              }else{
+                  var collectList=[];
+                  for(var t=0;t<content.length;t++){
+                    for(var i=0;i<doc1.length;i++){
+                      if(content[t].mallId==doc1[i].mallId){
+                          collectList[t]=doc1[i]
+                      }
+                    }
+                  }
+                  res.json({
+                    status: "0",
+                    msg: "查询成功",
+                    result: collectList
+                });
+              }
           })
-          for(var i=0;i<content.length;i++){
-            //   imgList.push(content[i].img)
-            // malls.findOne(param, function(err, doc) {})
-        }
-        res.json({
-            status: "0",
-            msg: "查询成功",
-            result: imgList
-        });
     })
 });
 
--- routes/malls.js
+++ routes/malls.js
@@ -4,6 +4,7 @@ var malls = require('./../models/mall')
 var multer = require('multer');
 let fs = require("fs");
 let path = require("path");
+var Collections = require('./../models/collection')
 
 // 上传模板商城图片
 router.post('/uploadImg', function(req, res, next) {
@@ -33,6 +34,7 @@ router.post('/uploadImg', function(req, res, next) {
 router.get('/getImgList', function(req, res, next) {
     let pageSize = req.param("pageSize")
     let currentPage = req.param("currentPage")
+    let Collect = {userName:req.param("userName")}
     let skip = (currentPage-1)*pageSize//从哪一条开始查询
     let totol;//总条数
     malls.find().exec({},function(err0,doc0){
@@ -40,21 +42,65 @@ router.get('/getImgList', function(req, res, next) {
         // 这个需要pageSize转换为数字，因为传过来成了字符串
         let allMall = malls.find().skip(skip).limit(Number(pageSize))
         allMall.exec({},function(err1,doc1){
+            // 需要查询下当前用户有没有收藏该模板，如果有的话要返回给用户
+            
+            Collections.findOne(Collect, function(err2, doc2) {
+                // console.log(doc2.resumeList,"doc2")
+                var list=[];
+                for(var t=0;t<doc2.resumeList.length;t++){
+                    for(var i=0;i<doc1.length;i++){
+                            if(doc2.resumeList[t].mallId==doc1[i].mallId){
+                            console.log("已经收藏",doc1[i].mallId)
+                            doc1[i].collectText="已经收藏"
+                            
+                        }else{
+                            console.log("收藏",doc1[i].mallId)
+                            doc1[i].collectText="收藏"
+                        }
+                        list=list.concat(doc1[i])
+                        // console.log(list,"list")
+                    }
+                }
+            //     doc2.resumeList.forEach((item,index)=>{
+            //          doc1.forEach((item1,index1)=>{
+            //             if(item.mallId==item1.mallId){
+            //                 console.log("已经收藏",item.mallId)
+            //                 item1.collectText="已经收藏"
+                            
+            //             }else{
+            //                 console.log("收藏",item.mallId)
+            //                 item1.collectText="收藏"
+            //             }
+            //             list.concat(1)
+            //             console.log(list,"list")
+
+            // })
+                    
+            // })
+            
             if (err1) {
                 res.json({
                     status: "1",
-                    msg: "获取失败" + err1
+                    msg: "获取失败"
                 });
             } else {
                 res.json({
                     status: "0",
                     msg: "获取成功",
                     result:{
-                        list:doc1,
+                        list:list,
                         totol:totol
                     }
                 });
             }
+            })
+            
+            
+            // doc1.forEach((item,index)=>{
+
+
+            // })
+            
         })
     })
  
